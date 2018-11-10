c5228d9fca3f0338cac28500441d3a2bcd34b48d
Darren <zen_darren@hotmail.com>
2018-11-10 14:35:21 +0800

    Update EmergencyRoom.java

--- PS1-B/EmergencyRoom.java
+++ PS1-B/EmergencyRoom.java
@@ -1,8 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-// write your matric number here: A0155600R
-// write your name here: Darren Chin
 // write list of collaborators here: 
 // year 2018 hash code: tPW3cEr39msnZUTL2L5J (do NOT delete this line)
 
